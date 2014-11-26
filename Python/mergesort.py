d961fffb9a4c57687022db3931deda519e03ee90
Zack_Smith <zack.smith@temple.edu>
2014-11-26 00:36:39 -0500

    Removed nested learning folders.

---
+++ Python/mergesort.py
@@ -0,0 +1,27 @@
+''' 
+Function which generates a random integer array, and sorts it with the merge sort algorithm.
+Inputs: None
+Outputs: Sorted random list
+Written by Zack Smith 11/26/14
+'''
+import random
+
+
+
+def merge(left, right, omega):
+    ''' Function which merges two input lists and writes it to output list omega.'''
+    
+    pass
+
+def split(omega):
+    ''' Function which takes large list (omega) and splits in to two smaller lists. '''
+    pass
+
+def main():
+    ''' Main function which sets up the program & calls important functions. '''
+    pass
+
+
+# Boiler plate code
+if __name__ == '__main__':
+    main()
