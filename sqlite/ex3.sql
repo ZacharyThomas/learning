74e8301a08e40b9ad4c6a29143bceb41cc10e144
Zachary.smith <Zachary.smith@yodle.com>
2017-07-15 19:40:24 -0400

    clean up

--- sqlite/ex3.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO person (id,first_name,last_name,age)
-       VALUES (0,"zed","shaw",37);
-
-INSERT INTO pet (id,name,breed,age,dead)
-       VALUES (0,"Fluffly","Unicorn",1000,0);
-
-INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1,1);
